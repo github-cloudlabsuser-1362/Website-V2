4dcbc0bf5927040e5327f1ef468782488dbbad14
github_cloudlabsuser_1362@cloudlabsaiuser.com <github_cloudlabsuser_1362@cloudlabsaiuser.com>
2025-06-12 14:44:58 +0000

    Initial commit

---
+++ ContosoTraders.Ui.Website.V2.Raw/ContosoTraders.Ui.Website.V2/src/components/AboutUs.js
@@ -0,0 +1,13 @@
+// AboutUs.jsx
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Welcome to our website! We are passionate about ...</p>
+    </div>
+  );
+}
+
+export default AboutUs;
